23cf2016a3e205038484d7c28cf9fac2b2ecc33a
SIDHARTH <SIDHARTH@DESKTOP-ULV4RC9>
2021-06-29 17:17:34 +0530

    new test case created

---
+++ src/test/java/com/actitime/test/test_random.java
@@ -0,0 +1,13 @@
+package com.actitime.test;
+
+import org.testng.annotations.Test;
+
+public class test_random 
+{
+	@Test
+	public void random()
+	{
+		System.out.println("Random created");
+	}
+
+}
